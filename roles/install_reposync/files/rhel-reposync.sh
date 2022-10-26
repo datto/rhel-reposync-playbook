3db9fae59a717cca5f668ca121098782be695a66
Dalton Miner <dminer@datto.com>
2022-10-26 13:54:27 -0400

    Add retry logic to rhel-reposync.sh
    The reposync command has a fixed number of retries for *all* the
    packages it will download, at which point it will quit. We don't want to
    let it potentially retry indefinitely, but since we're downloading a lot
    of packages, it's liable to fail. Therefore, retry the reposync command
    itself several times.
    
    Co-authored-by: Neal Gompa <ngompa@datto.com>

--- roles/install_reposync/files/rhel-reposync.sh
+++ roles/install_reposync/files/rhel-reposync.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -u
 
 # Reposync RHEL repositories
 # Author: Neal Gompa <ngompa@datto.com>
@@ -7,6 +8,7 @@
 # REQUIRES: RHEL 9 and dnf-plugins-core
 
 downloaddir="${1:-${SYNC_DIR}}"
+max_retries="${2:-${MAX_TRIES}}"
 
 # We can't easily use the version ID from this because it includes the minor version
 #source /etc/os-release
@@ -24,6 +26,21 @@ repoids=("rhel-${VERSION_ID}-for-${basearch}-baseos-rpms"
 "codeready-builder-for-rhel-${VERSION_ID}-${basearch}-debug-rpms"
 "codeready-builder-for-rhel-${VERSION_ID}-${basearch}-source-rpms")
 
+#ARGS:
+# $1 - download path
+# $2 - repo
+function do-reposync {
+	for (( i=1; i<=${MAX_TRIES}; i++ )); do
+		dnf reposync --download-path ${1} --download-metadata --repo="${2}"
+		if [ $? -eq 0 ]; then
+			return 0
+		elif [ $i -eq $MAX_TRIES ]; then
+			echo "MAX_TRIES reached; reposync has failed."
+			return $?
+		fi
+	done
+}
+
 for repoid in ${repoids[@]}; do
 	if [[ "$repoid" =~ "baseos" ]]; then
 		repodir="BaseOS"
@@ -33,16 +50,18 @@ for repoid in ${repoids[@]}; do
 		repodir="CodeReady-Builder"
 	fi
 	basedir="$downloaddir/RHEL/${VERSION_ID}"
+	ln_cmd="ln --symbolic --relative --force --no-target-directory --verbose"
+	mkdir -p ${basedir}/${basearch}/.mirror/
 	mkdir -p ${basedir}/${basearch}/${repodir}/
-	mkdir -p ${basedir}/Source/
+	mkdir -p ${basedir}/Source/.mirror
 	if [[ "$repoid" =~ "debug" ]]; then
-		dnf reposync -p ${basedir}/${basearch}/${repodir} --download-metadata --repo="${repoid}"
-		mv ${basedir}/${basearch}/${repodir}/${repoid} ${basedir}/${basearch}/${repodir}/debug
+		do-reposync ${basedir}/${basearch}/.mirror ${repoid}
+		${ln_cmd} ${basedir}/${basearch}/.mirror/${repoid} ${basedir}/${basearch}/${repodir}/debug
 	elif [[ "$repoid" =~ "source" ]]; then
-		dnf reposync -p ${basedir}/Source/ --download-metadata --repo="${repoid}"
-		mv ${basedir}/Source/${repoid} ${basedir}/Source/${repodir}
+		do-reposync ${basedir}/Source/.mirror ${repoid}
+		${ln_cmd} ${basedir}/Source/.mirror/${repoid} ${basedir}/Source/${repodir}
 	else
-		dnf reposync -p ${basedir}/${basearch}/${repodir}/ --download-metadata --repo="${repoid}"
-		mv ${basedir}/${basearch}/${repodir}/${repoid} ${basedir}/${basearch}/${repodir}/os
+		do-reposync ${basedir}/${basearch}/.mirror ${repoid}
+		${ln_cmd} ${basedir}/${basearch}/.mirror/${repoid} ${basedir}/${basearch}/${repodir}/os
 	fi
 done
